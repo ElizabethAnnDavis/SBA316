e2a8713b9a680c015032dc48dd1c9b373d5f757a
ElizabethAnnDavis <immakitty88@gmail.com>
2024-12-14 00:37:34 -0500

    changed save button to new button

--- app.js
+++ app.js
@@ -24,8 +24,8 @@
  *  3. 10% - Iterate over a collection of elements to accomplish some task.
  *  4.  5% - Create at least one element using createElement. ✓
  *  5.  5% - Use appendChild and/or prepend to add new elements to the DOM. ✓
- *  6. 10% - Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
- *  7.  5% - Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+ *  6. 10% - Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent. ✓
+ *  7.  5% - Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties. ✓
  *  8.  3% - Modify at least one attribute of an element in response to user interaction. ✓
  *  9. 10% - Register at least two different event listeners and create the associated event handler functions. ✓
  * 10.  3% - Use at least two Browser Object Model (BOM) properties or methods.
@@ -35,7 +35,7 @@
  *           but should include event-based validation in addition to the HTML attribute validation.)
  * 13. 10% - Ensure that the program runs without errors 
  *           (comment out things that do not work, and explain your blockers - you can still receive partial credit).
- * 14.  5% - Commit frequently to the git repository. (30x min) CURRENT: 8
+ * 14.  5% - Commit frequently to the git repository. (30x min) CURRENT: 10
  * 15.  5% - Level of effort displayed in creativity, presentation, and user experience.
  * 
  * 9% unaccounted for due to removed requirments
@@ -61,8 +61,8 @@ const treeTrunk = document.createElement('div');
 treeTrunk.classList.add("treeTrunk");
 
 const btnCont = document.querySelector('.btnCont');
-const saveBtn = document.createElement('button');
-saveBtn.innerHTML = "SAVE";
+const newBtn = document.createElement('button');
+newBtn.innerHTML = "Try Again?";
 const chngColorBtn = document.createElement('button');
 chngColorBtn.innerHTML = "Change Tree Color?";
 
@@ -78,7 +78,7 @@ function resetPage(e){
     createTree();
 
     btnCont.appendChild(chngColorBtn)
-    btnCont.appendChild(saveBtn);
+    btnCont.appendChild(newBtn);
     
     treeContainer.appendChild(mouseCircle);
 }
@@ -181,3 +181,8 @@ function changeTreeColor(e){
     chngColorBtn.innerHTML = "Change color again?";
 }
 chngColorBtn.addEventListener('click', changeTreeColor);
+
+function newImage(e){
+    
+}
+newBtn.addEventListener('click', newImage);
