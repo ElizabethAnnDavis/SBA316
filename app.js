e1a7ab78a34d0716572346ab95ef6d8bca2d85ad
ElizabethAnnDavis <immakitty88@gmail.com>
2024-12-14 23:25:35 -0500

    code cleaned up; some comments added

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
  *           but should include event-based validation in addition to the HTML attribute validation.)
  * 13. 10% - Ensure that the program runs without errors ✓
  *           (comment out things that do not work, and explain your blockers - you can still receive partial credit).
- * 14.  5% - Commit frequently to the git repository. (30x min) --> CURRENT COUNT: 25
+ * 14.  5% - Commit frequently to the git repository. (30x min) --> CURRENT COUNT: 28
  * 15.  5% - Level of effort displayed in creativity, presentation, and user experience. ✓
  * 
  * 9% unaccounted for due to removed requirments ?
